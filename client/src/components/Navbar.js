b5221bd4de07e30bfd9744d4a1cfb674f1e051dc
Tareq-dev <tareque.dev@gmail.com>
2023-06-12 20:21:05 +0600

    added base url

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link,  useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "./context/auth";
 import { CgLogOff } from "react-icons/cg";
 
